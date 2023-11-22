537c8bff9b932efd84e36f3655ddc001f3a8ee49
arifrizqy <m.arifrizqy.f@gmail.com>
2023-11-22 15:11:40 +0700

    mencoba laravel component

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,10 @@
+@extends('partials/app')
+@section('content')
+
+   <x-tag-title text="Home | MARKAS"/>
+
+</head>
+<body>
+   <h1>Home</h1>
+   
+@endsection()
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/home', function () {
-    return view('home');
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/home', 'index');
 });
 
 Route::get('/checkout', function () {
