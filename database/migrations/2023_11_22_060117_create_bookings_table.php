5a6d77b81c8c383f8f06b9f05bfcbb495774e268
rosyidmaulana <ocidbang728@gmail.com>
2023-11-22 13:48:10 +0700

    update

--- database/migrations/2023_11_22_060117_create_bookings_table.php
+++ database/migrations/2023_11_22_060117_create_bookings_table.php
@@ -11,14 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('bookings', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('is_admin');
-            $table->rememberToken();
+            $table->string('gender');
+            $table->integer('phone');
+            $table->string('company');
+            $table->string('event_type');
+            $table->string('event_name');
+            $table->string('description');
+            $table->integer('approval');
             $table->timestamps();
         });
     }
@@ -28,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bookings');
     }
 };
