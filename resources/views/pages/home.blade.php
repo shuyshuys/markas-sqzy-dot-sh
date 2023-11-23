acfc2817282d407af63f2e51cc09d3f1b4c5b782
arifrizqy <m.arifrizqy.f@gmail.com>
2023-11-23 12:48:16 +0700

    ubah home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,118 +3,192 @@
 
 <x-tag-title text="Home | MARKAS"/>
 
+<div class="py-5 px-20 flex justify-between">
+  
+  <a href="index.html" class="text-3xl text-[#D90027] font-semibold">MARKAS</a>
+   
+  <ul class="flex gap-10 text-xl text-[#D90027]">
+  
+    <li>
+      <a href="/home">Home</a>
+    </li>
+
+    <li>
+      <a href="/about">About</a>
+    </li>
+
+    <li>
+      <a href="/philosophy">Philosophy</a>
+    </li>
+
+     <li>
+      <a href="/events">Events</a>
+    </li>
+
+    <li>
+      <a href="/booking">Booking</a>
+    </li>
+
+    <li>
+      <a href="/contact">Contact Us</a>
+    </li>
+  </ul>
+</div>
+
+</div>
+
+<div class="bg-black py-40">
+<h1 class="text-7xl text-center font-semibold text-white">
+  MARKAS
+</h1>
+<p class="text-2xl text-center text-white mt-10">
+  A hub for startup founders to grow with the ecosystem.
+</p>
+</div>
+
 <div class="bg-[#D90027]">
-   <h1 class="text-white text-xl py-5 font-semibold text-center">INI HEADER SEMENTARA</h1>
- </div>
+<h1 class="text-white text-3xl py-5 font-semibold text-center">GAIN KNOWLEDGE - GET MENTORED - EXPAND NETWORK</h1>
+</div>
 
- <div class="bg-black py-40">
-   <h1 class="text-5xl text-center text-white">
-     MARKAS
-   </h1>
-   <p class="text-xl text-center text-white mt-4">
-     A hub for startup founders to grow with the ecosystem.
-   </p>
- </div>
+<div class="px-20">
+<div class="mt-20">
+  <h1 class="font-bold text-4xl">IKUTI KEGIATAN KAMI DI MARKAS</h1>
+  <p class="text-lg pt-4 pb-8">Jangan lewatkan event-event seru di MARKAS</p>
+</div>
 
- <div class="bg-[#D90027]">
-   <h1 class="text-white text-xl py-5 font-semibold text-center">GAIN KNOWLEDGE - GET MENTORED - EXPAND NETWORK</h1>
- </div>
+<div class="w-full flex gap-10 justify-center">
+  <div class="w-3/12">
+    <div style="background-image: url({{ asset('img/rectangle1.png') }});" class="w-full h-52 bg-cover border-2 border-solid border-black">
+    </div>
+    
+    <div class="border-2 border-solid border-black">
+    <h1 class="text-lg font-bold text-center my-2">SEKOLAH BETA HUSTLER <br> EPS. 39 - SCRUM FRAMEWORK</h1>
+    </div>
 
- <div class="px-20">
-   <div>
-     <h1 class="font-bold text-4xl">IKUTI KEGIATAN KAMI DI MARKAS</h1>
-     <p class="text-base pt-3">Jangan lewatkan event-event seru di MARKAS</p>
-   </div>
- 
+    <div class="bg-[#D90027] text-center text-sm flex text-slate-200 justify-between border-2 border-solid border-black">
+      <h1 class="mx-2 my-2">SURABAYA</h1>
+      <h1  class="mx-2 my-2">SEPTEMBER 2023</h1>
+    </div>
+  </div>
 
-   <div>
-     <h1 class="text-2xl text-[#D90027] text-center font-semibold mb-4">
-       FILOSOFI
-     </h1>
-     <h1 class="text-4xl text-center font-bold mb-5">INKLUSIF dan KOLABORATIF</h1>
-     <p class="text-center">Sejak diinisiasi di tahun 2016, semangat #1000StartupDigital adalah untuk berkolaborasi <br> membuat ekosistem startup Indonesia menjadi inklusif dan kolaboratif.</p>
-   </div>
+  <div class="w-3/12">
+    <div style="background-image: url({{ asset('img/rectangle1.png') }});" class="w-full h-52 bg-cover border-2 border-solid border-black">
+    </div>
+    
+    <div class="border-2 border-solid border-black">
+    <h1 class="text-lg font-bold text-center my-2">SEKOLAH BETA HUSTLER <br> EPS. 39 - SCRUM FRAMEWORK</h1>
+    </div>
 
-    <div class="text-center">
-       <button class="backbutton py-2 rounded-2xl bg-[#D90027] text-white px-8 text-lg mt-14 mb-6">
-       Cari Tahu Lebih
-       </button>
-     </div>
-   </div>
+    <div class="bg-[#D90027] text-center text-sm flex text-slate-200 justify-between border-2 border-solid border-black">
+      <h1 class="mx-2 my-2">SURABAYA</h1>
+      <h1  class="mx-2 my-2">SEPTEMBER 2023</h1>
+    </div>
+  </div>
 
-   <div class="bg-[#D90027]">
-     <h1 class="text-white text-3xl py-5 font-bold text-center">
-       MARI BERKOLABORASI
-     </h1>
+  <div class="w-3/12">
+    <div style="background-image: url({{ asset('img/rectangle1.png') }});" class="w-full h-52 bg-cover border-2 border-solid border-black">
+    </div>
+    
+    <div class="border-2 border-solid border-black">
+    <h1 class="text-lg font-bold text-center my-2">SEKOLAH BETA HUSTLER <br> EPS. 39 - SCRUM FRAMEWORK</h1>
+    </div>
 
-     <div class="text-center">
-       <button class="py-2 rounded-2xl bg-white text-[#D90027] px-12 text-lg mt-2 mb-7 font-semibold">
-       Booking
-       </button>
-     </div>
-   </div>
+    <div class="bg-[#D90027] text-center text-sm flex text-slate-200 justify-between border-2 border-solid border-black">
+      <h1 class="mx-2 my-2">SURABAYA</h1>
+      <h1  class="mx-2 my-2">SEPTEMBER 2023</h1>
+    </div>
+  </div>
+</div>
 
-   <div class="text-center">
-     <h1 class="text-xl font-semibold text-[#D90027]">KARAKTERISTIK</h1>
-     <h1 class="text-2xl font-bold">10 KARAKTER MARKAS</h1>
-     <p>Sejak diinisiasi di tahun 2016, semangat #1000StartupDigital adalah untuk berkolaborasi <br> membuat ekosistem startup Indonesia menjadi inklusif dan kolaboratif.</p>
-   </div>
 
-   <div class="text-center">
-     <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-2 font-semibold border-black  border-2">
-       🚀 VISIONARY
-     </button>
+<div>
+  <h1 class="text-2xl text-[#D90027] text-center font-semibold mb-4">
+    FILOSOFI
+  </h1>
+  <h1 class="text-4xl text-center font-bold mb-5">INKLUSIF dan KOLABORATIF</h1>
+  <p class="text-center">Sejak diinisiasi di tahun 2016, semangat #1000StartupDigital adalah untuk berkolaborasi <br> membuat ekosistem startup Indonesia menjadi inklusif dan kolaboratif.</p>
+</div>
 
-     <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-2 font-semibold border-black  border-2">
-       👟️ EXTRAMILE
-     </button>
+ <div class="text-center">
+    <button class="backbutton py-2 rounded-2xl bg-[#D90027] text-white px-8 text-lg mt-14 mb-6">
+    Cari Tahu Lebih
+    </button>
+  </div>
+</div>
 
-     <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-2 font-semibold border-black  border-2">
-       🧠 STRATEGIC
-     </button>
-   </div>
+<div class="bg-[#D90027]">
+  <h1 class="text-white text-3xl py-5 font-bold text-center">
+    MARI BERKOLABORASI
+  </h1>
 
-   <div class="text-center">
-     <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-3  font-semibold border-black  border-2">
-       🏆 GRIT
-     </button>
+  <div class="text-center">
+    <button class="py-2 rounded-2xl bg-white text-[#D90027] px-12 text-lg mt-2 mb-7 font-semibold">
+    Booking
+    </button>
+  </div>
+</div>
 
-     <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-3  font-semibold border-black  border-2">
-       ⚖ INTEGRITY
-     </button>
+<div class="text-center">
+  <h1 class="text-xl font-semibold text-[#D90027]">KARAKTERISTIK</h1>
+  <h1 class="text-2xl font-bold">10 KARAKTER MARKAS</h1>
+  <p>Sejak diinisiasi di tahun 2016, semangat #1000StartupDigital adalah untuk berkolaborasi <br> membuat ekosistem startup Indonesia menjadi inklusif dan kolaboratif.</p>
+</div>
 
-     <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-3  font-semibold border-black  border-2">
-       🎯 FOCUSED
-     </button>
+<div class="text-center">
+  <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-2 font-semibold border-black  border-2">
+    🚀 VISIONARY
+  </button>
 
-     <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-3  font-semibold border-black  border-2">
-       🚅 AGILE
-     </button>
-   </div>
+  <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-2 font-semibold border-black  border-2">
+    👟️ EXTRAMILE
+  </button>
 
-   <div class="text-center">
-     <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-3  font-semibold border-black  border-2">
-       🔥 PASSION
-     </button>
+  <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-2 font-semibold border-black  border-2">
+    🧠 STRATEGIC
+  </button>
+</div>
 
-     <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-3  font-semibold border-black  border-2">
-       📚 RESOURCEFUL
-     </button>
+<div class="text-center">
+  <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-3  font-semibold border-black  border-2">
+    🏆 GRIT
+  </button>
 
-     <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-3  font-semibold border-black  border-2">
-       🎨 CREATIVE
-     </button>
+  <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-3  font-semibold border-black  border-2">
+    ⚖ INTEGRITY
+  </button>
 
-   </div>
+  <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-3  font-semibold border-black  border-2">
+    🎯 FOCUSED
+  </button>
 
-   <div class="text-center">
-       <button class="backbutton py-2 rounded-2xl bg-[#D90027] text-white px-8 text-lg mt-12 mb-6">
-       Cari Tahu Lebih
-       </button>
-     </div>
+  <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-3  font-semibold border-black  border-2">
+    🚅 AGILE
+  </button>
+</div>
 
-   <div class="mt-40">
+<div class="text-center">
+  <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-3  font-semibold border-black  border-2">
+    🔥 PASSION
+  </button>
 
-   </div>
+  <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-3  font-semibold border-black  border-2">
+    📚 RESOURCEFUL
+  </button>
+
+  <button class="py-1 rounded-2xl bg-white text-black px-8 text-lg mt-3  font-semibold border-black  border-2">
+    🎨 CREATIVE
+  </button>
+
+</div>
+
+<div class="text-center">
+    <button class="backbutton py-2 rounded-2xl bg-[#D90027] text-white px-8 text-lg mt-12 mb-6">
+    Cari Tahu Lebih
+    </button>
+  </div>
+
+<div class="mt-40">
+
+</div>
 
 @endsection()
